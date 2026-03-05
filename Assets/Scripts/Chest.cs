a9260ab4e170fa09a8c2ffb8be1ad4f1e49c48c6
danghai-07 <pndanghai@gmail.com>
2026-03-05 22:44:48 +0700

    Add drop coin for chest

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -5,9 +5,13 @@ public class Chest : MonoBehaviour
     private Animator animator;
     private bool playerInRange = false;
     private bool isOpened = false;
-    public MonoBehaviour playerMovement;
 
+    public MonoBehaviour playerMovement;
     public GameObject questionPanel;
+
+    [Header("Drop Gold")]
+    public GameObject coinPrefab;
+    public int goldDropAmount = 5;
 
     void Start()
     {
@@ -29,8 +33,27 @@ public class Chest : MonoBehaviour
     {
         questionPanel.SetActive(false);
         animator.SetTrigger("Open");
-        isOpened = true;
+        DropGold();
         playerMovement.enabled = true;
+    }
+
+    void DropGold()
+    {
+        if (coinPrefab == null) return;
+
+        for (int i = 0; i < goldDropAmount; i++)
+        {
+            Vector2 randomOffset = new Vector2(
+                Random.Range(-0.5f, 0.5f),
+                Random.Range(0f, 0.5f)
+            );
+
+            Instantiate(
+                coinPrefab,
+                (Vector2)transform.position + randomOffset,
+                Quaternion.identity
+            );
+        }
     }
 
     public void WrongAnswer()
