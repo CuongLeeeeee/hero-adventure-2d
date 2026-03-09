fd828182702624621539a9d43ebf9f285407f7ce
Le Truong Cuong <89770922+CuongLeeeeee@users.noreply.github.com>
2026-03-09 15:23:30 +0700

    fix: fix animation patrol enemy

--- Assets/Art/Characters/Hero Knight - Pixel Art/_Demo/HeroKnight.cs
+++ Assets/Art/Characters/Hero Knight - Pixel Art/_Demo/HeroKnight.cs
@@ -173,6 +173,7 @@ public class HeroKnight : MonoBehaviour
             h.GetComponent<GolemEnemy>()?.TakeDamage(m_attackDamage);
             h.GetComponent<BatEnemy>()?.TakeDamage(m_attackDamage);
             h.GetComponent<SkeletonEnemy>()?.TakeDamage(m_attackDamage);
+            h.GetComponent<PatrolEnemy>()?.TakeDamage(m_attackDamage);
             h.GetComponent<FireDemon>()?.TakeDamage(m_attackDamage);
         }
     }
